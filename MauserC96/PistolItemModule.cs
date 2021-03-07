06918735cb2e3cce0deff34140a74bfedc343b30
Lil Husky VR <lilhuskyvr@gmail.com>
2021-03-07 22:42:23 +1100

    save

---
+++ MauserC96/PistolItemModule.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using ThunderRoad;
+using UnityEngine;
+
+namespace MauserC96
+{
+    public class PistolItemModule : ItemModule
+    {
+        public string bulletItemId = "AOTBullet", muzzleEffectId;
+        public float bulletSpeed = 100;
+
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+
+            item.gameObject.AddComponent<Pistol>();
+        }
+    }
+
+    public class Pistol : MonoBehaviour
+    {
+        private Item _pistolItem;
+        private Transform _bulletSpawner;
+        private Transform _shellSpawner;
+        private Transform _muzzle;
+        private bool _isShootCoroutineRunning;
+        private bool _isSpawnBulletCoroutineRunning;
+        private Item _bulletItem;
+        private PistolItemModule _pistolItemModule;
+        private ItemData _bulletData;
+        private EffectData _muzzleEffectData;
+
+        private void Start()
+        {
+            _pistolItem = GetComponent<Item>();
+            _pistolItemModule = _pistolItem.data.GetModule<PistolItemModule>();
+
+            LoadCustomReferences();
+            RegisterEvents();
+            LoadCatalogData();
+        }
+
+        private void LoadCustomReferences()
+        {
+            _bulletSpawner = _pistolItem.GetCustomReference("BulletSpawner");
+            _shellSpawner = _pistolItem.GetCustomReference("ShellSpawner");
+            _muzzle = _pistolItem.GetCustomReference("Muzzle");
+        }
+
+        private void RegisterEvents()
+        {
+            _pistolItem.OnHeldActionEvent += PistolItemOnOnHeldActionEvent;
+        }
+
+        private void LoadCatalogData()
+        {
+            _bulletData = Catalog.GetData<ItemData>(_pistolItemModule.bulletItemId);
+            _muzzleEffectData = Catalog.GetData<EffectData>(_pistolItemModule.muzzleEffectId);
+        }
+
+        private void PistolItemOnOnHeldActionEvent(RagdollHand ragdollhand, Handle handle, Interactable.Action action)
+        {
+            StartCoroutine(SpawnBulletCoroutine());
+            if (action == Interactable.Action.UseStart)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+
+        public IEnumerator Shoot()
+        {
+            if (_isShootCoroutineRunning)
+            {
+                Debug.Log("Shoot is running");
+                yield return null;
+            }
+            else
+            {
+                if (_bulletItem == null)
+                    yield return SpawnBulletCoroutine();
+
+                //wait for the bullet to spawn
+                while (_bulletItem == null)
+                {
+                    yield return new WaitForFixedUpdate();
+                }
+                
+                _isShootCoroutineRunning = true;
+
+                _bulletItem.transform.SetParent(null, true);
+                _bulletItem.transform.localScale = _pistolItem.transform.localScale;
+                _bulletItem.rb.isKinematic = false;
+                _bulletItem.SetColliderAndMeshLayer(GameManager.GetLayer(LayerName.MovingObject));
+                _bulletItem.rb.detectCollisions = true;
+                _bulletItem.rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+                // //add smoke
+                // var smokeEffect = _smokeData.Spawn(_bulletItem.transform.Find("TailIndicator"));
+                // smokeEffect.SetIntensity(10);
+                // smokeEffect.Play();
+                //missile start flying
+                _bulletItem.rb.AddForce(_pistolItemModule.bulletSpeed * _bulletItem.flyDirRef.forward,
+                    ForceMode.Impulse);
+                _bulletItem.isThrowed = true;
+                _bulletItem.isFlying = true;
+                _bulletItem.disallowDespawn = false;
+                //forget it
+                _bulletItem = null;
+                _isShootCoroutineRunning = false;
+                yield return null;
+            }
+        }
+
+        public IEnumerator SpawnBulletCoroutine()
+        {
+            if (_isSpawnBulletCoroutineRunning || _bulletItem != null)
+            {
+                yield return null;
+            }
+            else
+            {
+                Debug.Log("Start Spawning Bullet");
+                _isSpawnBulletCoroutineRunning = true;
+                //spawn bullet
+                _bulletData.SpawnAsync(resultItem =>
+                {
+                    resultItem.disallowDespawn = true;
+                    resultItem.IgnoreObjectCollision(_pistolItem);
+                    resultItem.rb.isKinematic = true;
+                    resultItem.transform.position = _bulletSpawner.transform.position;
+                    resultItem.transform.rotation = _bulletSpawner.transform.rotation;
+                    resultItem.transform.SetParent(_bulletSpawner, true);
+                    _bulletItem = resultItem;
+                    Debug.Log("Spawned bullet");
+                });
+
+                while (_bulletItem == null)
+                {
+                    yield return new WaitForFixedUpdate();
+                }
+
+                _isSpawnBulletCoroutineRunning = false;
+
+                yield return null;
+            }
+        }
+    }
+}
---
+++ MauserC96/PistolTestingLevelModule.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IngameDebugConsole;
+using ThunderRoad;
+using UnityEngine;
+
+// ReSharper disable UnusedMember.Global
+
+namespace MauserC96
+{
+    public class PistolTestingLevelModule : LevelModule
+    {
+        private Item _pistolItem;
+        private Item _pistolBullet;
+        private Creature _testCreature;
+
+        public override IEnumerator OnLoadCoroutine(Level level)
+        {
+            DebugLogConsole.AddCommandInstance("m96s",
+                "Spawn A Pistol", "SpawnPistol",
+                this);
+            
+            DebugLogConsole.AddCommandInstance("m96b",
+                "Spawn A Pistol", "SpawnBullet",
+                this);
+
+            DebugLogConsole.AddCommandInstance("m96n",
+                "Navigate", "NavigatePistol",
+                this);
+
+            DebugLogConsole.AddCommandInstance("m96f",
+                "Fire", "Fire",
+                this);
+
+            DebugLogConsole.AddCommandInstance("m96nf",
+                "Navigate & Fire", "NavigateFire",
+                this);
+
+            DebugLogConsole.AddCommandInstance("m96c",
+                "Test Creature", "SpawnTestCreature",
+                this);
+
+            DebugLogConsole.AddCommandInstance("m96d",
+                "TestDecal", "TestDecal",
+                this);
+
+            return base.OnLoadCoroutine(level);
+        }
+
+        public void SpawnPistol()
+        {
+            Catalog.GetData<ItemData>("AOTPistol").SpawnAsync(item =>
+            {
+                item.transform.position = Player.local.transform.position + 3f * Player.local.transform.forward +
+                                          2 * Player.characterData.height * Vector3.up;
+
+                item.transform.rotation = Player.local.creature.transform.rotation;
+
+                item.rb.isKinematic = true;
+
+                _pistolItem = item;
+            });
+        }
+
+        public void NavigatePistol()
+        {
+            foreach (var creature in Creature.list)
+            {
+                if (!creature.isPlayer && creature.state == Creature.State.Alive)
+                {
+                    _pistolItem.transform.LookAt(creature.ragdoll.headPart.transform);
+                }
+            }
+        }
+
+        public void Fire()
+        {
+            GameManager.local.StartCoroutine(_pistolItem.gameObject.GetComponent<Pistol>().Shoot());
+        }
+
+        public void NavigateFire()
+        {
+            NavigatePistol();
+            Fire();
+        }
+
+        public override void Update(Level level)
+        {
+            // if (_testCreature)
+            // {
+            //     foreach (Creature.RendererData renderer in _testCreature.renderers)
+            //     {
+            //         if ((bool) renderer.revealDecal && renderer.revealDecal.type == RevealDecal.Type.Body)
+            //             renderer.revealDecal.Reset();
+            //     }
+            // }
+        }
+
+        public void SpawnTestCreature()
+        {
+            GameManager.local.StartCoroutine(Catalog.GetData<CreatureData>("HumanFemale").SpawnCoroutine(
+                Player.local.transform.position + Player.local.transform.forward, Player.local.transform.rotation, null,
+                creature =>
+                {
+                    creature.SetFaction(2);
+                    (creature.brain.instance as BrainHuman).canLeave = false;
+                    _testCreature = creature;
+                }));
+        }
+
+        public void TestDecal()
+        {
+            // GameManager.local.StartCoroutine(Catalog.GetData<CreatureData>("HumanFemale").SpawnCoroutine(
+            //     Player.local.transform.position + Player.local.transform.forward, Player.local.transform.rotation, null,
+            //     creature =>
+            //     {
+            //         creature.SetFaction(5);
+            //         creature.brain.Load("BaseWarrior");
+            //         creature.brain.instance.Start();
+            //         (creature.brain.instance as BrainHuman).canLeave = false;
+            //     }));
+
+
+            var hitInfo = new RaycastHit();
+            Physics.Raycast(new Ray(_testCreature.transform.position + _testCreature.transform.forward,
+                _testCreature.ragdoll.headPart.transform.position -
+                (_testCreature.transform.position + _testCreature.transform.forward)
+            ), out hitInfo, 10, (1 << 27 | 1 << 0 | 1 << 13));
+
+            Debug.Log("Hit infor");
+            Debug.Log(hitInfo.point);
+            
+            var freeindex = _testCreature.ragdoll.headPart.collisionHandler.GetFreeCollisionIndex();
+            _testCreature.ragdoll.headPart.collisionHandler.collisions[freeindex].NewHit(
+                _pistolItem.colliderGroups[0].colliders[0],
+                hitInfo.collider,
+                _pistolItem.colliderGroups[0],
+                _testCreature.ragdoll.headPart.colliderGroup,
+                new Vector3(100, 100, 100),
+                hitInfo.point,
+                hitInfo.normal,
+                100,
+                Catalog.GetData<MaterialData>("Blade"),
+                Catalog.GetData<MaterialData>("Flesh")
+            );
+
+            var effect = Catalog.GetData<EffectData>("HitProjectileOnFlesh")
+                .Spawn(hitInfo.point, Quaternion.identity);
+            effect.SetTarget(_testCreature.ragdoll.headPart.transform);
+            effect.SetIntensity(10);
+            effect.Play();
+        }
+    }
+}
